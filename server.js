b12e3351252f1ea0e45837bf6455d42de94e9ab6
Jaxgod <sandrinadupan12@gmail.com>
2025-03-29 13:05:03 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const path = require('path');
+const qrcode = require('qrcode'); 
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => {res.sendFile(path.join(__dirname, 'public', 'index.html','pair'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.get('/qr-code', (req, res) => {
+  const whatsappLink = 'https://web.whatsapp.com/';   qrcode.toDataURL(whatsappLink, (err, url) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la génération du QR Code');
+    } else {
+ res.json({ qrCodeUrl: url });
+    }
+  });
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
