09e56ec8d9474e31e09b47af70e3043300fcf357
Jaxgod <sandrinadupan12@gmail.com>
2025-03-29 09:50:30 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public','pair')));
 app.get('/', (req, res) => {res.sendFile(path.join(__dirname, 'public', 'index.html','pair'));
 });
 const PORT = process.env.PORT || 3000;
